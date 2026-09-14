149452024a2c8ee8b29d8f086e352bb297f2abc5
Sabrina <161891067+Mangle707@users.noreply.github.com>
2026-09-14 16:20:43 -0300

    atual

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 <body>
     <?php
       $nome = "Sabrina";
+      $idade = 19;
       echo "<h1>Ola, $nome </h1>";
-   echo "<p>Este é um exemplo simples de uma página construída apenas com HTML puro.</p>";
-    
- 
-
+      echo "<p>Este é um exemplo simples de uma página construída apenas com HTML puro.</p>";
         ?>
+        <h1>Nome: <?= $nome ?></h1>
+        <p>Idade: <?= $idade ?></p>
 </body>
 </html>
