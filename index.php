751a821b4722144bd468bf72b557f3fe9e3bc762
Sabrina <161891067+Mangle707@users.noreply.github.com>
2026-09-14 16:56:44 -0300

    colocando style

--- index.php
+++ index.php
@@ -4,21 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
       $nome = "Sabrina";
       $idade = 19;
-         
+           
       if ($idade >= 18){
-        $maiorid = "Sim voce é";
+        $maiorid = "Sim voce é"; //nova variavel para calcular sua idade
       }
       else {
         $maiorid = "Nao é";
       }
         ?>
+        <div classname=container>
         <h1>Nome: <?= $nome ?></h1>
-        <p>Idade: <?= $idade ?></p>
+        <h1>Idade: <?= $idade ?></h1>
         <p>E maior de idade? <?= $maiorid ?></p>
+        </div>
 </body>
 </html>
