c6354eea55173db902fd7e7a7f5c74bfc85d554c
Sabrina <161891067+Mangle707@users.noreply.github.com>
2026-09-15 14:11:29 -0300

    a little upd

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>...</title>
      <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -21,6 +21,7 @@
         <div classname=container>
         <h1>Nome: <?= $nome ?></h1>
         <h1>Idade: <?= $idade ?></h1>
+        <hr>
         <p>E maior de idade? <?= $maiorid ?></p>
         </div>
 </body>
