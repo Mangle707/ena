04e7964a861633b025692bffde70b81708f140bd
Sabrina <161891067+Mangle707@users.noreply.github.com>
2026-09-21 16:56:07 -0300

    Update NotasDesafio.php

--- NotasDesafio.php
+++ NotasDesafio.php
@@ -33,7 +33,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atividade Notas(2)</title>
+    <title>NotasDesafio(2)</title>
     <link rel="stylesheet" href="style/notas.css">
 </head>
 <body>
