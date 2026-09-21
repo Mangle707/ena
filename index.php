2f6cc136cb3c276391a7960d73078de69b34e70b
Sabrina <161891067+Mangle707@users.noreply.github.com>
2026-09-21 14:22:33 -0300

    atualizaçao imagem
    adciona imagem de fundo e titulo novo

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>...</title>
+    <title>Delta</title>
      <link rel="stylesheet" href="style.css">
 </head>
 <body>  
