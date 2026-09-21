2e01977b086d9adfa4cf432f6183fdf97c66a80c
Sabrina <161891067+Mangle707@users.noreply.github.com>
2026-09-21 16:23:36 -0300

    mais css e umas correçoes no codigo

--- notas.php
+++ notas.php
@@ -34,7 +34,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atividade Notas(2)</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="style/notas.css">
 </head>
 <body>
 <div class="container">
