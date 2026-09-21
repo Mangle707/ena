3e125b8d1cc49a98b0cd135f8ce6b090a2912e8e
Sabrina <161891067+Mangle707@users.noreply.github.com>
2026-09-21 15:24:54 -0300

    style para a pasta

--- idade.php
+++ idade.php
@@ -21,7 +21,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atividade idade(1)</title>
-     <link rel="stylesheet" href="style.css">
+     <link rel="stylesheet" href="style/idade.css">
 </head>
 <body>  
         
